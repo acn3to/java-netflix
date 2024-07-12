d5834959c11ea9f1547df36d2a28beff6aa8f57e
KI0T0 <f_g@outlook.com.br>
2024-07-12 19:55:06 -0300

    feat: add Category User

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,4 +1,77 @@
 package entities;
 
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isAdmin;
+
+
+    public User(int id, String name, String email, String password, boolean isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if (id > 0) {
+            this.id = id;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.trim().isEmpty()) {
+            this.name = name;
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email != null && email.contains("@")) {
+            this.email = email;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password != null && password.length() >= 8) {
+            this.password = password;
+        }
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+
+    @Override
+    public String toString() {
+        return
+                "ID: " + id +
+                "\nName: " + name + '\'' +
+                "\nE-mail: " + email +
+                "\nIs an administrator:" + isAdmin;
+    }
 }
