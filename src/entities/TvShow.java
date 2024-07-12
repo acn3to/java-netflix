58fa31114cf54bf9a89ee4af86e853095fcf319a
Leticia Borchardt <leticiaborchardt123@gmail.com>
2024-07-12 19:50:49 -0300

    feat: add attributes, getters and setters to TvShow class

--- src/entities/TvShow.java
+++ src/entities/TvShow.java
@@ -1,4 +1,31 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TvShow extends Media {
+    private Map<Integer, List<String>> seasons;
+
+    public TvShow() {
+        this.seasons = new HashMap<Integer, List<String>>();
+    }
+
+    public TvShow(Map<Integer, List<String>> seasons) {
+        this.seasons = seasons;
+    }
+
+    public TvShow(int id, String title, String description, String director, LocalDate releaseDate, Category category, double rating, Map<Integer, List<String>> seasons) {
+        super(id, title, description, director, releaseDate, category, rating);
+        this.seasons = seasons;
+    }
+
+    public Map<Integer, List<String>> getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(Map<Integer, List<String>> seasons) {
+        this.seasons = seasons;
+    }
 }
