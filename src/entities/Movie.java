e7433961c6b59fc652cf2e93cc09eca201148912
Leticia Borchardt <leticiaborchardt123@gmail.com>
2024-07-12 19:47:33 -0300

    feat: add attributes, getters and setters to Movie class

--- src/entities/Movie.java
+++ src/entities/Movie.java
@@ -1,4 +1,26 @@
 package entities;
 
-public class Movie extends Media{
+import java.time.LocalDate;
+
+public class Movie extends Media {
+    private int durationInMinutes;
+
+    public Movie() {}
+
+    public Movie(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public Movie(int id, String title, String description, String director, LocalDate releaseDate, Category category, double rating, int durationInMinutes) {
+        super(id, title, description, director, releaseDate, category, rating);
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
 }
